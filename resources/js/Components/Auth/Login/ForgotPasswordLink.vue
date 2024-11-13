0314b3eaaece17de1a16f0e1933df21aa6a8ea7e
Richie van der Heij <Richievanderheij@gmail.com>
2024-11-13 15:34:35 +0100

    Feat - Organize folders + Small change - Changed link text and structured SCSS imports for better organization.

--- resources/js/Components/Auth/Login/ForgotPasswordLink.vue
+++ resources/js/Components/Auth/Login/ForgotPasswordLink.vue
@@ -5,6 +5,6 @@ import { Link } from '@inertiajs/vue3';
 <template>
   <div class="forgot-password">
     <i class="forgot-password__icon fas fa-lock"></i>
-    <Link :href="route('password.request')" class="forgot-password__link">Forgot your password?</Link>
+    <Link :href="route('password.request')" class="forgot-password__link">Forgot password?</Link>
   </div>
 </template>
