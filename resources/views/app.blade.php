384e9c9a51d19f107297b602ffd290ee02f76a71
Richie van der Heij <Richievanderheij@gmail.com>
2025-02-11 15:04:14 +0100

    Feat - Enhance cookie consent management by adding Google Consent Mode and Cookie Declaration script in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,11 +10,42 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    <!-- Google Consent Mode -->
+    <script data-cookieconsent="ignore">
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag("consent", "default", {
+            ad_personalization: "denied",
+            ad_storage: "denied",
+            ad_user_data: "denied",
+            analytics_storage: "denied",
+            functionality_storage: "denied",
+            personalization_storage: "denied",
+            security_storage: "granted",
+            wait_for_update: 500,
+        });
+        gtag("set", "ads_data_redaction", true);
+        gtag("set", "url_passthrough", false);
+    </script>
+
     <!-- Cookiebot Script -->
-    <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js"
-            data-cbid="eb5a8a84-458d-49ac-ba40-eee70b46fe14"
-            data-blockingmode="auto"
-            type="text/javascript"></script>
+    <script
+        id="Cookiebot"
+        src="https://consent.cookiebot.com/uc.js"
+        data-cbid="eb5a8a84-458d-49ac-ba40-eee70b46fe14"
+        data-blockingmode="auto"
+        type="text/javascript">
+    </script>
+
+    <!-- Cookie Declaration Script -->
+    <script
+        id="CookieDeclaration"
+        src="https://consent.cookiebot.com/eb5a8a84-458d-49ac-ba40-eee70b46fe14/cd.js"
+        type="text/javascript"
+        async>
+    </script>
 
     <!-- Scripts -->
     @routes
