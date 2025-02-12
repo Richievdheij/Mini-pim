e8a6ffce32a26bafe4fd9fa2a1f1c43dbd93b87a
Richie van der Heij <Richievanderheij@gmail.com>
2025-02-12 13:42:24 +0100

    Feat - CookieBot Update

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,5 +1,18 @@
 <script setup>
+import { onMounted } from 'vue';
 import Footer from '@/Components/Layout/Footer.vue';
+
+onMounted(() => {
+    const existingScript = document.getElementById('CookieDeclaration');
+    if (!existingScript) {
+        const script = document.createElement('script');
+        script.id = 'CookieDeclaration';
+        script.src = 'https://consent.cookiebot.com/eb5a8a84-458d-49ac-ba40-eee70b46fe14/cd.js';
+        script.type = 'text/javascript';
+        script.async = true;
+        document.body.appendChild(script);
+    }
+});
 </script>
 
 <template>
